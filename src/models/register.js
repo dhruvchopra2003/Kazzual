eb7c79d404eb487c931b9c7a9b49d8b88b8eaa40
Falcon <dhruvchopra2019@gmail.com>
2024-02-05 05:33:36 +0530

    Commiting the some data and the filters, needs UI updation

--- src/models/register.js
+++ src/models/register.js
@@ -1,64 +1,64 @@
-const mongoose=require("mongoose");
-const bcrypt=require("bcryptjs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const kazualinfo=new mongoose.Schema({
-    Username:{
-        type:String,
-    
-    },    
-    Email:{
-        type:String,
-        unique:true,
+const kazualinfo = new mongoose.Schema({
+    Username: {
+        type: String,
+
     },
-   Password:{
-        type:String,
+    Email: {
+        type: String,
+        unique: true,
     },
-    Contact:{
-        type:Number,
+    Password: {
+        type: String,
+    },
+    Contact: {
+        type: Number,
     },
     Location: {
-        type:String,
+        type: String,
     },
-    Detail:{
-        type:String,        
+    Detail: {
+        type: String,
 
     },
-    Sports:{
-        type:String,
+    Sports: {
+        type: String,
     },
-    Birthday:{
-        type:String,
+    Birthday: {
+        type: String,
     },
-    Age:{
-        type:Number,
+    Age: {
+        type: Number,
     },
-    Name:{
-        type:String,
+    Name: {
+        type: String,
     },
-    Timing:{
-        type:String,
+    Timing: {
+        type: String,
     },
-    Image:{
-        type:String,
+    Image: {
+        type: String,
     }
-   
+
 });
 
 
-kazualinfo.pre("save",async function(next){
-    if(this.isModified('Password')){
-        try{
-        this.Password= await bcrypt.hash(this.Password,12);
-        
+kazualinfo.pre("save", async function (next) {
+    if (this.isModified('Password')) {
+        try {
+            this.Password = await bcrypt.hash(this.Password, 12);
+
+        }
+
+        catch (e) {
+            console.log("error hashing password", e);
+        }
     }
-    
-    catch(e){
-         console.log("error hashing password",e);
-    }
-}
     next();
 })
 
-const Register=new mongoose.model("Register",kazualinfo);
+const Register = new mongoose.model("Register", kazualinfo);
 
-module.exports=Register;
+module.exports = Register;
